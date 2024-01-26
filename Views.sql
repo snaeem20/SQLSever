e46d4badc076ebdb7dfe5e8a99e3c236b22b3da1
snaeem20 <156692041+snaeem20@users.noreply.github.com>
2024-01-26 11:35:23 +0500

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,23 +1,64 @@
+---------------------------------------------------------------
+VIEWS
+---------------------------------------------------------------
 CREATE VIEW VW_SelectedFaculty AS
+
 SELECT CRS.CourseName, FAC.FacultyName, FAC.FacultyQualification 
 FROM Faculty AS FAC
 LEFT OUTER JOIN Course AS CRS ON CRS.FacultyID = FAC.FacultyID
+GO
 
 
 SELECT * FROM VW_SelectedFaculty
+Order by CourseName
 
-
---If the same user-defined error is raised at multiple locations,
---using a unique state number for each location can help find which section of code is raising the errors.
---Severity levels from 20 through 25 are considered fatal
-
-RAISERROR ('This is message %s %d.', -- Message text.
-           25, -- Severity,
-           8, -- State,
-           'number', -- First argument.
-           5); -- Second argument.
 
 GO
+
+CREATE TABLE AuditLogType
+(
+Id INT NOT NULL PRIMARY KEY IDENTITY,
+LogType VARCHAR(100) NULL
+)
+GO
+
+--DROP TABLE AuditLogType
+--DROP TABLE AuditLog
+
+Select * from AuditLog
+
+CREATE VIEW VW_SelectAuditData
+AS
+SELECT TableAction FROM AuditLog
+UNION
+SELECT LogType FROM AuditLogType
+
+Select * from VW_SelectAuditData
+
+INSERT INTO VW_SelectAuditData
+VALUES ('Deleted')
+
+
+CREATE VIEW VW_SelectAuditDataFromSingleTable
+AS
+SELECT TableAction FROM AuditLog
+
+
+INSERT INTO VW_SelectAuditDataFromSingleTable
+VALUES ('Deleted')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ---------------------------------------------------------------
 --Program Flow Statements
